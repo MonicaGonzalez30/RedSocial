f1f5a31d9d39dbdd8d6162e814103a2f67b36cd0
MonicaGonzalez30 <monik196901@gmail.com>
2021-10-10 19:56:23 -0500

    Modificacion final del register

--- Frontend/js/register.js
+++ Frontend/js/register.js
@@ -1,32 +1,26 @@
 async function createUser() {
-    let nombre = document.getElementById('name').value;
-    apellidoPat = document.getElementById('apePat').value;
-    apellidoMat = document.getElementById('apeMat').value;
-    emai = document.getElementById('email').value;
-    passw = document.getElementById('password').value;
-
-    if(nombre == null || nombre.length == 0 || /^\s+$/.test(nombre)) {
-        alert('ERROR: El campo nombre debe llenarse.');
-        return false;
-    } else if(apellidoPat == null || apellidoPat.length == 0 || /^\s+$/.test(apellidoPat)) {
-        alert('ERROR: El campo apellido paterno debe llenarse.');
-        return false;
-    } else if(apellidoMat == null || apellidoMat.length == 0 || /^\s+$/.test(apellidoMat)) {
-        alert('ERROR: El campo apellido materno debe llenarse.');
-        return false;
-    } else if(emai == null || emai.length == 0 || /^\s+$/.test(emai)) {
-        alert('ERROR: El campo email debe llenarse.');
-        return false;
-    } else if(passw == null || passw.length < 8 || /^\s+$/.test(passw)) {
-        alert('ERROR: El campo contraseña debe llenarse y tener mínimo 8 caracteres.');
-        return false;
-    }
-
     let name = document.getElementById('name').value;
     let apePat = document.getElementById('apePat').value;
     let apeMat = document.getElementById('apeMat').value;
     let email = document.getElementById('email').value;
     let pass = document.getElementById('password').value;
+
+    if(name == null || name.length == 0 || /^\s+$/.test(name)) {
+        alert('ERROR: El campo nombre debe llenarse.');
+        return false;
+    } else if(apePat == null || apePat.length == 0 || /^\s+$/.test(apePat)) {
+        alert('ERROR: El campo apellido paterno debe llenarse.');
+        return false;
+    } else if(apeMat == null || apeMat.length == 0 || /^\s+$/.test(apeMat)) {
+        alert('ERROR: El campo apellido materno debe llenarse.');
+        return false;
+    } else if(email == null || email.length == 0 || /^\s+$/.test(email)) {
+        alert('ERROR: El campo email debe llenarse.');
+        return false;
+    } else if(pass == null || pass.length < 8 || /^\s+$/.test(pass)) {
+        alert('ERROR: El campo contraseña debe llenarse y tener mínimo 8 caracteres.');
+        return false;
+    }
 
     let usuario = {
         name: name,
